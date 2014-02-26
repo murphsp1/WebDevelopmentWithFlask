5269b95c310e9bb8086face28d9b5940d9a398a5
Sean Murphy <murphsp1@gmail.com>
2014-02-26 11:01:11 -0500

    chapter 2 done

--- environment_setup.md
+++ environment_setup.md
@@ -15,4 +15,10 @@ Now it is time to load up flask and install into the venv
 
     pip install flask
 
-    
+
+## Flask contexts that are available: ##
+
+current_app (Application context) - the application instance for the active application
+g (Application context) - a Python dictionary for the app for temporary storage during the handling of a request (reset with each request)
+request [request context] - the request object that encapsulates the contents of a HTTP request sent by the client.
+session [request context] - the user session, a dictionary that the app can use to store values that are remembered between requests.
--- flasky.py
+++ flasky.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, abort
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
 
@@ -8,14 +10,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return '<h1>Hello World!</h1>'
+	user_agent = request.headers.get('User-Agent')
+	return '<p>Your browser is %s</p>' % user_agent
+
+
+@app.route('/response')
+def response():
+	response = make_response('<h1>This document carries a cookie!</h1>')
+	response.set_cookie('answer','42')
+	return response
+
+@app.route('/redirect_test')
+def redirect_test():
+	return redirect('http://www.google.com/')
 
 
 @app.route('/user/<name>')
 def user(name):
+	if not name:
+		abort(404)
 	return '<h1>Hello, %s!</h1>' % name
 
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	#app.run(debug=True)
+	manager.run()
