b25839df72092f5d15d21c216be6e636e0dfda4d
Sean Murphy <murphsp1@gmail.com>
2014-02-26 09:45:47 -0500

    first little sample program done

---
+++ environment_setup.md
@@ -0,0 +1,10 @@
+cd to project root
+
+
+    virtualenv venv
+
+This setups the virtual environment named venv
+
+     source venv/bin/activate
+
+This runs the activate 
---
+++ flasky.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+#Routes for this test application
+
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s!</h1>' % name
