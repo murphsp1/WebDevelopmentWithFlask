f0ac7f8943873ada9a7d4e97891975a90d50d803
Sean Murphy <murphsp1@gmail.com>
2014-02-26 13:00:28 -0500

    chapter 4 finished

--- flasky_chapter4.py
+++ flasky_chapter4.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form 
@@ -24,6 +24,9 @@ class NameForm(Form):
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
+		old_name = session['name']
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
 		session['name'] = form.name.data
 		form.name.data=''
 		return redirect(url_for('index'))
