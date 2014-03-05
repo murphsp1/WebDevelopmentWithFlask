4a2dc1a1a4246e236e7af596778214279f63baeb
Sean Murphy <murphsp1@gmail.com>
2014-03-05 17:00:34 -0500

    added a new project to carve up the slate_bootstrap theme into flask

---
+++ NewLook/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+
+@app.route('/')
+def index():
+	return render_template('index_slate.html')
+
+
+if __name__ == '__main__':
+	app.run(debug = True)
+
+
